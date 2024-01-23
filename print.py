22c37af3308e8724366bbb659ff991f20347dc60
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-01-23 11:43:20 -0600

    Completed Assigment

---
+++ print.py
@@ -0,0 +1,4 @@
+# \n \t \\ \' \" end= sep=
+# knock knock joke about person who wants to use the bathroom really badly 
+
+print("*Knock, Knock.\nWho's there?\nAnita\nAnita who?\nAnita use the bathroom, please open the door!")
