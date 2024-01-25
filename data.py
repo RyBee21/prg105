76fa067d60936f6a53a636cbde2bb08fb6e55a10
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-01-25 11:06:24 -0600

    Create data.py

---
+++ data.py
@@ -0,0 +1,24 @@
+# Var a that hold a value of 30 
+# Function prints a Literal value of 30 
+a = 30
+print("Literal:", a)
+
+# Var b that holds a value of -90
+# Function prints the Integer -90
+b = -90
+print("Integer", b)
+
+# Var c that holds the value 999.999
+# Function prints the Float 999.99
+c = 999.99
+print("Float:", c)
+
+# Var animal that holds that string Raccoon
+# Function prints the String Raccoon 
+animal = "Raccoon"
+print("String:", animal)
+
+# Var is_deactive that holds boolnean the expression false 
+# Function prints Boolean expression false
+is_deactive = False
+print("Boolean:", is_deactive)
