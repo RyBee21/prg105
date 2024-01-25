ce70e2e5a5efc5da9b80b71e0aec9c8f4106c853
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-01-25 11:40:36 -0600

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,9 @@
+# Stores length and width 
+length = 5
+width = 4 
+
+#calculates area 
+area = length * width
+
+#prints area 
+print("The area of the rectangle with a length of " + str(length) + " and a width of " + str(width) + " is " + str(area))
