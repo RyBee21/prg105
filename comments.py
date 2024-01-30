3992fbf3bf7a7f4150f0aae1834cdcd4d67d0baf
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-01-30 11:44:54 -0600

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,1 +1,14 @@
+# Tank size of vehicle in gallons 
+tank_size = 15 
+
+'''Distance needed to travel '''
+distance = 600
+
+"""Caluclating the miles per gallon by taking the distance traveled
+then dividing it by the size of the tank"""
+mpg = distance / tank_size 
+
+print("miles per gallon:", mpg) # Prints an output of the current mpg 
+
+
 
