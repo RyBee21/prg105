2ef64564d9952e80bdb9f49b89b53462153dd561
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-02-06 13:10:22 -0600

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,27 @@
+# Determines the freedoms unlocked to the individual 
+
+# Finds the users age 
+
+age = int(input("How old are you?"))
+            
+# if-else statements 
+            
+if age >= 16:
+    print("Here are your keys.")
+else:
+    print("sorry you are not old enough")
+
+if age >= 18:
+    print("Who would you like to vote for?")
+else:
+    print("Come back next time kid.")
+
+if age >= 21:
+    print("Would you like to buy a shot?")
+else:
+    print("You're not old enough go fight for a war or something.")
+
+if age >= 65:
+    print("Yoou qualify for the senior discount!")
+else:
+    print("You do not qualify for the senior discount")
