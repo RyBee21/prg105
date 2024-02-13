7f0a0afcbd7c94ff2ce10a7f116acf51c1866059
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-02-13 11:38:14 -0600

    Create list_operations.py

---
+++ list_operations.py
@@ -0,0 +1,9 @@
+# Ticket sales
+
+seats_available = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 , 20]
+
+while seat 0!
+for seat in seats_available:
+    print(seat)
+    seats_available.pop(input("What seat would you like to purchase?   Answer 1-20"))
+    
