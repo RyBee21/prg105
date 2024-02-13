07aba97bc6979c811cfc798de24b3e4c5083f6ac
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-02-13 13:05:47 -0600

    Update Bubbble_lists.py

--- Bubbble_lists.py
+++ Bubbble_lists.py
@@ -1,28 +1,27 @@
-# Program that sorts user generated list with bubble sort
+# Program that sorts user-generated list with bubble sort
 
-# Accepts 5 names from user and stores it in names list 
+# Accepts 5 names from the user and stores it in names list 
 names = []
 for i in range (1,5):
     names.append(input("What names would you like to write down on the list?"))
-print(names)
 
 # Sorts list using  Bubble Sort algorithm 
-# Flag to track if a swap has occured 
+# Flag to track if a swap has occurred 
 swapped = True 
 
 # Loops until the list is swapped 
 while swapped:
     swapped = False 
-    for i in range(len(names - 1)):
-        if numbers[i] > numbers[i + 1]:
+    for i in range(len(names) - 1 ):
+        if names[i] > names[i + 1]:
             swapped = True 
-            names[i], names[i + 1] = numbers(i + 1), numbers[i]
+            names[i], names[i + 1] = names[i + 1], names[i]
 
-# Prints bubble sort algorith output 
+# Prints bubble sort algorithm output 
 print(names)
 
 # Reverses sorted list
 names.reverse()
 
-#prints the reveresed list 
+# print the reversed list 
 print(names)
