8275a6089fccd1be1438e3df5f995ecf602706e2
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-02-13 14:46:08 -0600

    Update list_operations.py

--- list_operations.py
+++ list_operations.py
@@ -1,9 +1,24 @@
 # Ticket sales
 
-seats_available = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 , 20]
+# Seats currently for sale 
+seats_available = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 
-while seat 0!
-for seat in seats_available:
-    print(seat)
-    seats_available.pop(input("What seat would you like to purchase?   Answer 1-20"))
-    
+# flag that checks if the shopper is done 
+shopping = "None"
+
+# Loops the user a question to see if they want to purchase a seat 
+while shopping != "0":
+    for seat in seats_available:
+        print(seat)
+
+# Promts user to enter their purchase 
+    shopping = input("What would you like? type 0 if finished:")
+  
+# Removes a seat if it has been purchased 
+    if shopping in seats_available:
+        seats_available.remove(shopping)
+
+# Checks to see if the place is sold out 
+    if len(seats_available) == 0:
+            print("You are done!") # Lets user know they are done
+            shopping = "0" # Exits the program
