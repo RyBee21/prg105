76c698f2420c9f493417991b740b77dbd9be5c4a
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-02-27 11:22:58 -0600

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# Creating a tuple
+def main():
+    programming_classes = ("Advanced Python", "Database Essentials", "Webdevelopment basics", 
+                           "Data structures in python", "Web Design Fundemantals")
+    print("There are ",len(programming_classes)," classes")
+    for classes in programming_classes:
+        print(classes)
+      
+
+# Call Main 
+main()
