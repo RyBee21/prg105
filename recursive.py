1423d0047b2513b942ec87a4ca438f0a8d30e7b2
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-02-27 11:08:14 -0600

    Create recursive.py

---
+++ recursive.py
@@ -0,0 +1,16 @@
+# Factorial function 
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+# Main Function
+def main(n):
+    n = int(input("Please enter a non negative integer? "))
+    factorial_ans = factorial(n)
+    print(f"The factorail of {n} is {factorial_ans}")
+
+# Calls Function
+main()
+    
