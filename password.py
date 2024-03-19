057d86b99717f259a79bb980fdae0a66d0a98461
RyBee21 <156958949+RyBee21@users.noreply.github.com>
2024-03-19 11:28:11 -0500

    Create password.py

---
+++ password.py
@@ -0,0 +1,36 @@
+def main():
+    valid = False  # change to true if all condions are met
+    while not valid:
+        valid = True  # we will change to false if ANY requirement not met
+        print("""Password Requirements:\n
+            Between 8 to 20 characters long.\n
+            Contains at least one uppercase letter.\n
+            Contains at least one lowercase letter.\n
+            Includes at least one number.\n
+            Includes at least one symbol from the set: !@#$%&*\n""")
+
+        password = input("Please enter a password that meets the criteria: ")
+        length = len(password)
+        if 7 < length < 21:
+            continue
+        else:
+            valid = False
+            print("That password is not the right length")
+
+        is_upper = False  # change to true if found
+        # for loop stepping through characters in password. Look for an upper case.
+
+        has_symbol = False
+        symbol = ['!', '@', '#']
+        for s in symbol:
+            for c in password:
+                if s == c:
+                    has_symbol == True
+        if has_symbol == False:
+            print("you need to include a symbol")
+            valid = False
+            continue
+        
+
+
+main()
